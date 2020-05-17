d79e17eadb50138fc0a52e3a6ec2a90bde9d523d
Jorge Antônio da Silva Nascimento Junior <jorgenjr@outlook.com>
2020-05-17 11:56:16 -0300

    First Commit Ever

---
+++ ntalesbot.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			switch update.Message.Command() {
+			case "help":
+				msg.Text = "type /sayhi or /status."
+			case "sayhi":
+				msg.Text = "Hi :)"
+			case "status":
+				msg.Text = "I'm ok."
+			default:
+				msg.Text = "I don't know that command"
+			}
+			bot.Send(msg)
+		}
+
+	}
+}
