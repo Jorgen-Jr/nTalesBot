41fb7a0eef78a2dbf7db52008616721d36203264
Jorge Antônio da Silva Nascimento Junior <jorgenjr@outlook.com>
2020-05-17 12:11:34 -0300

    :thinking: :thinking: :thinking:

--- src/controllers/requestController.go
+++ src/controllers/requestController.go
@@ -1,30 +1,8 @@
 package requestController
 
 import (
-	"github.com/0x111/telegram-rss-bot/models"
 	"strconv"
 )
-
-// This function replies the list of feeds to for the command /list
-func ListOfFeeds(botAPI *tgbotapi.BotAPI, feeds *[]models.Feed, chatid int64, messageid int) {
-	txt := "Here is the list of your added Feeds for this Room: \n"
-
-	if len(*feeds) == 0 {
-		txt += "There is currently no feed added to the list for this Room!\n"
-	}
-
-	for _, feed := range *feeds {
-		txt += "[#" + strconv.Itoa(feed.ID) + "] *" + feed.Name + "*: " + feed.Url + "\n"
-	}
-
-	msg := tgbotapi.NewMessage(chatid, txt)
-	msg.ReplyToMessageID = messageid
-
-	msg.ParseMode = "markdown"
-	msg.DisableWebPagePreview = true
-
-	botAPI.Send(msg)
-}
 
 func SimpleMessage(botAPI *tgbotapi.BotAPI, chatid int64, messageid int, text string) error {
 
